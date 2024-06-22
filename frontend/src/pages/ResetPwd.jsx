26e01628005634372738e448ea81b0a7047ac7c4
Yiping Tang <tangyiping_yvonne@163.com>
2024-06-22 23:14:23 +1000

    complete auth: register, login, forget pwd

--- frontend/src/pages/ResetPwd.jsx
+++ frontend/src/pages/ResetPwd.jsx
@@ -1,23 +1,27 @@
 import React from 'react';
-import { useNavigate } from 'react-router-dom';
+import { useNavigate, useLocation } from 'react-router-dom';
 import TextField from '@mui/material/TextField';
 import Button from '@mui/material/Button';
 import Typography from '@mui/material/Typography';
 import CardActions from '@mui/material/CardActions';
 import CardContent from '@mui/material/CardContent';
-import Link from '@mui/material/Link';
 
 import { CenteredBox, CenteredCard } from '../components/CenterBoxLog';
 import { apiCall } from '../helper';
 import MessageAlert from '../components/MessageAlert';
 import GradientBackground from '../components/GradientBackground';
+import ResetSuccess from './ResetSuccess.jsx';
 
 const ResetPwd = (props) => {
-  const [email, setEmail] = React.useState('');
   const [password, setPassword] = React.useState('');
   const [passwordConfirmed, setPasswordConfirmed] = React.useState('');
   const [unmatched, setUnmatched] = React.useState('');
+  const [success, setSuccess] = React.useState(false);
   const navigate = useNavigate();
+  const location = useLocation();
+  const [email, setEmail] = React.useState('');
+  const [token, setToken] = React.useState('');
+
   const [open, setOpen] = React.useState(false); // alert state
   const [snackbarContent, setSnackbarContent] = React.useState('');
   const [alertType, setAlertType] = React.useState('error');
@@ -29,13 +33,13 @@ const ResetPwd = (props) => {
     }
     setOpen(false);
   };
-
-  // load dashboard
-  React.useEffect(() => {
-    if (props.token) {
-      navigate('/');
-    }
-  }, [props.token]);
+  
+	React.useEffect(() => {
+		// get token, email from url
+		const params = new URLSearchParams(location.search);
+		setToken(params.get('token'));
+    setEmail(params.get('email'));
+	}, []);
 
   // check password validation
   React.useEffect(() => {
@@ -47,9 +51,9 @@ const ResetPwd = (props) => {
   }, [passwordConfirmed, password]);
 
   ///////////////////////
-  // register function: activate when click on the register button
+  // reset function: activate when click on the rest button
   /////////////////////// 
-  const register = async () => {
+  const reset = async () => {
     if (unmatched) {
       setSnackbarContent('Password unmatched');
       setAlertType('error');
@@ -65,30 +69,25 @@ const ResetPwd = (props) => {
       return 
     };
 
-    // check valid email
-    const regex = /^[^\s@]+@[^\s@]+\.[^\s@]+$/
-    if (! regex.test(email)){
-      setSnackbarContent('Invalid Email');
-      setAlertType('error');
-      setOpen(true);
-      return 
-    };
-
     // check valid password
     const passwordRegex = /^(?=.*[A-Za-z])(?=.*\d)(?=.*[@$!%*?&])[A-Za-z\d@$!%*?&]{8,}$/;
     if (! passwordRegex.test(password)){
-      setSnackbarContent('Password must be at least 8 characters long, including letter, number, and special character.');
+      setSnackbarContent('Password must be at least 8 characters long, including letter, number, and special character @$!%*?&.');
       setAlertType('error');
       setOpen(true);
       return 
     }
     
+    
     // try to request
     const requestBody = {
-      email, password
+      new_password: password,
+      new_password_confirm: passwordConfirmed,
+      token: token
     };
+
     try {
-      const data = await apiCall('POST', 'user/auth/register', requestBody);
+      const data = await apiCall('POST', 'v1/user/reset/password', requestBody);
       if (data.error) {
         setSnackbarContent(data.error);
         setAlertType('error');
@@ -98,54 +97,50 @@ const ResetPwd = (props) => {
         // localStorage.setItem('email', email);
         // props.setToken(data.token);
         // props.setEmail(email);
-        navigate('/verify-email-link-sent');
+        setSuccess(true);
         // setSnackbarContent('data.msg');
         // setAlertType('success');
         // setOpen(true);
       }
     } catch (error) {
-      console.error('Error during register:', error);
+      console.error('Error during reset password:', error);
     }
   };
 
   return (
     <>
-      <CenteredBox>
-        <CenteredCard>
-          <CardContent>
-            <Typography variant="h4" component="div">
-            Reset Password
-            </Typography> <br />
-            <Typography variant="body2">
-              <TextField id="email" label="Email" type="text" value={email} onChange={e => setEmail(e.target.value)} /> <br /><br />
-              <TextField id="password" label="Password" type="password" value={password} onChange={e => setPassword(e.target.value)} /> <br /><br />
-              <TextField id="passwordConfirmed" label="Password Confirmed" type="password" value={passwordConfirmed} onChange={e => setPasswordConfirmed(e.target.value)} />
-              <br /><br />
-            </Typography>
-          </CardContent>
-          <CardActions>
-            <Button id="buttonRegister" variant="contained" onClick={register} aria-label="Click me to register">Reset</Button>
-            {unmatched && <small id='unmatchError' style={{ color: 'red', paddingLeft: '1vw' }}>{unmatched}<br/></small>}
-          </CardActions>
-          <CardContent>
-          <div style={{ display: 'flex', justifyContent: 'center' }}>
-            <small>
-              Already have an account?{'\u00A0'}
-              <Link
-                href="#"
-                onClick={() => navigate('/login')}
-                aria-label="Click me to login page"
-              >
-                Login
-              </Link>
-              {'\u00A0'}now
-            </small>
-          </div>
-          </CardContent>
-        </CenteredCard>
-      </CenteredBox>
-      <MessageAlert open={open} alertType={alertType} handleClose={handleClose} snackbarContent={snackbarContent}/>
-      <GradientBackground />
+      {success ? <ResetSuccess /> :(
+        <>
+          <CenteredBox>
+            <CenteredCard>
+              <CardContent>
+                <Typography variant="h4" component="div">
+                Reset Password
+                </Typography> <br />
+                <Typography variant="body2">
+                  <TextField
+                    id="email"
+                    label="Email"
+                    type="text"
+                    value={email}
+                    onChange={e => setEmail(e.target.value)}
+                    disabled={true}
+                  /> <br /><br />
+                  <TextField id="password" label="Password" type="password" value={password} onChange={e => setPassword(e.target.value)} /> <br /><br />
+                  <TextField id="passwordConfirmed" label="Password Confirmed" type="password" value={passwordConfirmed} onChange={e => setPasswordConfirmed(e.target.value)} />
+                  <br /><br />
+                </Typography>
+              </CardContent>
+              <CardActions>
+                <Button id="buttonReset" variant="contained" onClick={reset} aria-label="Click me to reset">Reset</Button>
+                {unmatched && <small id='unmatchError' style={{ color: 'red', paddingLeft: '1vw' }}>{unmatched}<br/></small>}
+              </CardActions>
+            </CenteredCard>
+          </CenteredBox>
+          <MessageAlert open={open} alertType={alertType} handleClose={handleClose} snackbarContent={snackbarContent}/>
+          <GradientBackground />
+        </>
+      )}
     </>
   )
 }
