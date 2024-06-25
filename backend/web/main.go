63008a5e12d4544678586464ac825864cea6eb5b
Xue Cuiling <xuecuiling9909@gmail.com>
2024-06-25 23:04:47 +1000

    debug case-insensitive issues

--- backend/web/main.go
+++ backend/web/main.go
@@ -16,6 +16,9 @@ func main() {
 	initialize.InitDB()
 	
 	global.DB.AutoMigrate(&models.User{}, &models.Team{}, &models.Project{}, &models.Skill{})
+	// 修改现有表的字段排序规则
+    global.DB.Exec("ALTER TABLE skills MODIFY skill_name VARCHAR(255) COLLATE utf8mb4_bin")
+
 
 	Router := initialize.InitRouters()
 
