7225e227d6444dcf5060298aa1ba2bfff059ba41
avery shaw <averyshaw@averydeMacBook-Air.local>
2024-06-30 22:24:29 +1000

    regulate the fulllayout style

--- frontend/src/pages/Profile.jsx
+++ frontend/src/pages/Profile.jsx
@@ -6,16 +6,12 @@ import EditIcon from '@mui/icons-material/Edit';
 import SaveIcon from '@mui/icons-material/Save';
 import PhotoCamera from '@mui/icons-material/PhotoCamera';
 
+import '../assets/scss/FullLayout.css';//make sure import this
 import '../assets/scss/UserInfo.css';
 import Sidebar from "../layouts/Sidebar";
 import Header from "../layouts/Header";
 import { apiCall, fileToDataUrl } from '../helper';
 import MessageAlert from '../components/MessageAlert';
-
-const contentAreaStyle = {
-  marginTop: '56px', // Adjust this value to match the Header height
-  // padding: '16px', // Optional padding for the content area
-};
 
 const roleMap = {
   1: 'Student',
@@ -232,39 +228,23 @@ const handleFileChange = async (event) => {
     }
 };
 
-  const headerStyleLg = {
-    position: "fixed",
-    top: 0,
-    // width: "100%",
-    width: "calc(100% - 260px)",
-    zIndex: 1000,
-  };
-
-  const headerStyleMd = {
-    position: "fixed",
-    top: 0,
-    width: "100%",
-    // width: "calc(100% - 260px)",
-    zIndex: 1000,
-  };
-
   return (
     <main>
-    <div className="pageWrapper d-lg-flex">
-      {/********Sidebar**********/}
-      <aside className="sidebarArea shadow" id="sidebarArea">
-        <Sidebar />
-      </aside>
-      {/********Content Area**********/}
-      <div className="contentArea"  style={contentAreaStyle}>
-        <div className="d-mg-none" style={headerStyleLg}>
-                {/********Header**********/}
-                <Header />
-            </div>
-            <div className="d-lg-none" style={headerStyleMd}>
-                {/********Header**********/}
-                <Header/>
-            </div>
+      <div className="pageWrapper d-lg-flex">
+        {/********Sidebar**********/}
+        <aside className="sidebarArea shadow" id="sidebarArea">
+          <Sidebar />
+        </aside>
+        {/********Content Area**********/}
+        <div className="contentArea">
+          <div className="d-lg-none headerMd">
+            {/********Header**********/}
+            <Header />
+          </div>
+          <div className="d-none d-lg-block headerLg">
+            {/********Header**********/}
+            <Header />
+          </div>
         {/********Middle Content**********/}
         <Container className="p-4 wrapper" fluid>
                 <Box sx={{ display: 'flex', alignItems: 'center', gap: 3 }}>
