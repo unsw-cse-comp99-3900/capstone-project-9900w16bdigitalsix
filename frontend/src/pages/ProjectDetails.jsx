d7f2ef5fe725dc3ee430377ceaa2b49162214856
Desheng Liu <liudesheng1994@gmail.com>
2024-07-12 01:35:39 +1000

    project Details

--- frontend/src/pages/ProjectDetails.jsx
+++ frontend/src/pages/ProjectDetails.jsx
@@ -1,100 +1,107 @@
 import React, { useState, useEffect } from "react";
-import { useLocation } from "react-router-dom";
-import { Container, Card, CardTitle, CardBody, Button } from "reactstrap";
+import { useNavigate } from "react-router-dom";
+import { Container, Card, CardTitle, CardBody } from "reactstrap";
+import Button from "@mui/material/Button";
 import Dialog from "@mui/material/Dialog";
-import DialogContent from "@mui/material/DialogContent";
 import DialogTitle from "@mui/material/DialogTitle";
-import Avatar from "@mui/material/Avatar";
+import DialogContent from "@mui/material/DialogContent";
+import IconButton from "@mui/material/IconButton";
+import CloseIcon from "@mui/icons-material/Close";
+import Typography from "@mui/material/Typography";
+import List from "@mui/material/List";
+import ListItem from "@mui/material/ListItem";
+import Divider from "@mui/material/Divider";
+import ListItemText from "@mui/material/ListItemText";
 
 import Sidebar from "../layouts/Sidebar";
 import Header from "../layouts/Header";
 import "../assets/scss/FullLayout.css"; //make sure import this
-// import TutorAssign from "../components/AssignTeamTutorModal";
-// import CoorAssign from "../components/AssignTeamCoorModal";
 import { apiCall } from "../helper";
 
 const ProjectDetails = () => {
-  const location = useLocation();
-  const { item } = location.state || {};
+  // const ProjectDetails = ({projectId}) => {
+  const projectId = 7;
 
-  const {
-    clientEmail,
-    clientName,
-    description,
-    field,
-    projectId,
-    requiredSkills,
-    title,
-  } = item;
+  // some states
+  const [title, setTitle] = useState("");
+  const [clientName, setClientName] = useState("");
+  const [clientEmail, setClientEmail] = useState("");
+  const [field, setField] = useState("");
+  const [requiredSkills, setRequiredSkills] = useState([]);
+  const [description, setDiscription] = useState("");
+  const [specLink, setSpecLink] = useState("");
+  const [open, setOpen] = useState(false);
+  const [currentTeam, setCurrentTeam] = useState([]);
+  const navigate = useNavigate();
 
-  const [tutor, setTutor] = useState(null);
-  const [coordinator, setCoordinator] = useState(null);
-  // State for the Tutor Dialog
-  const [tutorDialogOpen, setTutorDialogOpen] = useState(false);
-  const toggleTutorDialog = () => {
-    setTutorDialogOpen(!tutorDialogOpen);
-    fetchTutor();
+  // this function is used to fetch a specific project detail
+  const getProjectDetail = async () => {
+    try {
+      const res = await apiCall("GET", `v1/project/detail/${projectId}`);
+      console.log(res);
+      if (res === null) {
+        return;
+      }
+      if (res.error) {
+        return;
+      } else {
+        setTitle(res.title);
+        setClientName(res.clientName);
+        setClientEmail(res.clientEmail);
+        setField(res.field);
+        setRequiredSkills(res.requiredSkills);
+        setDiscription(res.description);
+        setSpecLink(res.specLink);
+      }
+    } catch (error) {
+      return;
+    }
   };
-  // State for the Coordinator Dialog
-  const [coorDialogOpen, setCoorDialogOpen] = useState(false);
-  const toggleCoorDialog = () => {
-    console.log("coorDialogOpen", !coorDialogOpen);
-    fetchCoordinator();
-    setCoorDialogOpen(!coorDialogOpen);
-  };
-
-  const { tutorId, tutorName, tutorEmail, tutorAvatar } = tutor || {};
-  const { coorId, coorEmail, coorName, coorAvatar } = coordinator || {};
 
   useEffect(() => {
-    if (projectId) {
-      fetchTutor();
-      fetchCoordinator();
-    }
-  }, [projectId]);
+    getProjectDetail();
+  }, []);
 
-  const fetchTutor = async () => {
-    const token = localStorage.getItem("token");
-    const result = await apiCall(
-      "GET",
-      `v1/admin/get/tutor/${projectId}`,
-      null,
-      token,
-      true
-    );
-    if (!result.error) {
-      setTutor({
-        tutorId: result.tutorId,
-        tutorName: result.name,
-        tutorEmail: result.email,
-        tutorAvatar: result.avatarURL,
-      });
-    } else {
-      console.error("Error fetching tutor info:", result.error);
-      setTutor(null);
+  // this function used to get all teams that have already been allocated to a specific project
+  const getAllAllocatedTeams = async () => {
+    try {
+      const res = await apiCall(
+        "GET",
+        `v1/project/team/allocated/${projectId}`
+      );
+      if (res === null) {
+        setCurrentTeam([]);
+        return;
+      }
+      if (res.error) {
+        setCurrentTeam([]);
+      } else {
+        setCurrentTeam(res);
+      }
+    } catch (error) {
+      return;
     }
   };
 
-  const fetchCoordinator = async () => {
-    const token = localStorage.getItem("token");
-    const result = await apiCall(
-      "GET",
-      `v1/admin/get/coordinator/${projectId}`,
-      null,
-      token,
-      true
-    );
-    if (!result.error) {
-      setCoordinator({
-        coorId: result.coorId,
-        coorName: result.name,
-        coorEmail: result.email,
-        coorAvatar: result.avatarURL,
-      });
-    } else {
-      console.error("Error fetching coordinator info:", result.error);
-      setCoordinator(null);
-    }
+  const handleClick = () => {
+    getAllAllocatedTeams();
+    setOpen(true);
+  };
+
+  const handleClose = () => {
+    setOpen(false);
+  };
+
+  const handleClickTeam = (teamId, teamIdShow, teamName) => {
+    navigate("/team/preference", {
+      state: {
+        projectId: projectId,
+        title: title,
+        teamId: teamId,
+        teamIdShow: teamIdShow,
+        teamName: teamName,
+      },
+    });
   };
 
   return (
@@ -116,7 +123,7 @@ const ProjectDetails = () => {
           </div>
           {/********Middle Content**********/}
           <Container className="p-4 wrapper" fluid>
-            <Card>
+            <Card style={{ marginTop: "5%" }}>
               <CardTitle
                 tag="h5"
                 className="border-bottom p-3 mb-0"
@@ -129,7 +136,7 @@ const ProjectDetails = () => {
                 }}
               >
                 <div>
-                  {title}
+                  ProjectName: {title}
                   <div
                     style={{
                       fontSize: "14px",
@@ -139,6 +146,11 @@ const ProjectDetails = () => {
                   >
                     Project Id: {projectId}
                   </div>
+                </div>
+                <div>
+                  <Button variant="contained" onClick={handleClick}>
+                    Allocated Teams
+                  </Button>
                 </div>
               </CardTitle>
               <CardBody
@@ -182,6 +194,12 @@ const ProjectDetails = () => {
                     <strong>Field:</strong> {field}
                   </p>
                   <p
+                    style={{ margin: "0", fontSize: "16px", fontWeight: "400" }}
+                  >
+                    <strong>Required Skills:</strong>{" "}
+                    {requiredSkills ? requiredSkills.join(", ") : "N/A"}
+                  </p>
+                  <p
                     style={{
                       margin: "0",
                       fontSize: "16px",
@@ -191,232 +209,110 @@ const ProjectDetails = () => {
                   >
                     <strong>Description:</strong> {description}
                   </p>
-                  <p
-                    style={{ margin: "0", fontSize: "16px", fontWeight: "400" }}
-                  >
-                    <strong>Required Skills:</strong>{" "}
-                    {requiredSkills.join(", ")}
-                  </p>
                 </div>
-                <div>
-                  <h5
-                    style={{
-                      margin: "30px 30px 10px 30px",
-                      fontSize: "18px",
-                      fontWeight: "600",
-                    }}
-                  >
-                    Project Tutor
-                  </h5>
-                  {tutorId || tutorName || tutorEmail ? (
-                    <Card
-                      style={{
-                        display: "flex",
-                        flexDirection: "row",
-                        flexWrap: "wrap",
-                        justifyContent: "space-between",
-                        alignItems: "center",
-                        padding: "8px 20px",
-                        margin: "10px auto",
-                        boxShadow: "0 2px 8px rgba(0,0,0,0.1)",
-                        width: "70%",
-                        boxSizing: "border-box",
-                      }}
+                <div style={{ margin: "30px", fontSize: "14pt" }}>
+                  {specLink ? (
+                    <a
+                      href={specLink}
+                      rel="noopener noreferrer"
+                      target="_blank"
                     >
-                      <div style={{ minWidth: "60px", marginRight: "20px" }}>
-                        <Avatar
-                          src={tutorAvatar}
-                          alt="Coor Avatar"
-                          style={{ width: "60px", height: "60px" }}
-                          imgProps={{
-                            onError: (e) => {
-                              e.target.onerror = null;
-                              e.target.src =
-                                "https://example.com/default-avatar.png";
-                            },
-                          }}
-                        />
-                      </div>
-
-                      <div
-                        style={{
-                          flexGrow: 1,
-                          minWidth: "160px",
-                          flexBasis: "50%",
-                          display: "flex",
-                          flexDirection: "column",
-                          justifyContent: "center",
-                          alignItems: "flex-start",
-                        }}
-                      >
-                        <p
-                          style={{
-                            margin: "0",
-                            fontSize: "14px",
-                            fontWeight: "400",
-                          }}
-                        >
-                          <strong>Name:</strong> {tutorName}
-                          <br />
-                          <strong>Email:</strong> {tutorEmail}
-                        </p>
-                      </div>
-
-                      <Button
-                        color="primary"
-                        onClick={toggleTutorDialog}
-                        style={{
-                          whiteSpace: "nowrap",
-                          alignSelf: "center",
-                          padding: "6px 12px",
-                          fontSize: "14px",
-                        }}
-                      >
-                        Change
-                      </Button>
-                    </Card>
+                      Click here to download the project specification
+                    </a>
                   ) : (
-                    <Button
-                      color="primary"
-                      className="mb-3 btn-lg"
-                      style={{ display: "block", margin: "30px" }}
-                      onClick={toggleTutorDialog}
-                    >
-                      Assign Tutor
-                    </Button>
-                  )}
-                </div>
-                <div>
-                  <h5
-                    style={{
-                      margin: "30px 30px 10px 30px",
-                      fontSize: "18px",
-                      fontWeight: "600",
-                    }}
-                  >
-                    Project Coordinator
-                  </h5>
-                  {coorId || coorName || coorEmail ? (
-                    <Card
-                      style={{
-                        display: "flex",
-                        flexDirection: "row",
-                        flexWrap: "wrap",
-                        justifyContent: "space-between",
-                        alignItems: "center",
-                        padding: "8px 20px",
-                        margin: "10px auto",
-                        boxShadow: "0 2px 8px rgba(0,0,0,0.1)",
-                        width: "70%",
-                        boxSizing: "border-box",
-                      }}
-                    >
-                      <div style={{ minWidth: "60px", marginRight: "20px" }}>
-                        <Avatar
-                          src={coorAvatar}
-                          alt="Coor Avatar"
-                          style={{ width: "60px", height: "60px" }}
-                          imgProps={{
-                            onError: (e) => {
-                              e.target.onerror = null;
-                              e.target.src =
-                                "https://example.com/default-avatar.png";
-                            },
-                          }}
-                        />
-                      </div>
-
-                      <div
-                        style={{
-                          flexGrow: 1,
-                          minWidth: "160px",
-                          flexBasis: "50%",
-                          display: "flex",
-                          flexDirection: "column",
-                          justifyContent: "center",
-                          alignItems: "flex-start",
-                        }}
-                      >
-                        <p
-                          style={{
-                            margin: "0",
-                            fontSize: "14px",
-                            fontWeight: "400",
-                          }}
-                        >
-                          <strong>Name:</strong> {coorName}
-                          <br />
-                          <strong>Email:</strong> {coorEmail}
-                        </p>
-                      </div>
-
-                      <Button
-                        color="primary"
-                        onClick={toggleCoorDialog}
-                        style={{
-                          whiteSpace: "nowrap",
-                          alignSelf: "center",
-                          padding: "6px 12px",
-                          fontSize: "14px",
-                        }}
-                      >
-                        Change
-                      </Button>
-                    </Card>
-                  ) : (
-                    <Button
-                      color="primary"
-                      className="mb-3 btn-lg"
-                      style={{ display: "block", margin: "30px" }}
-                      onClick={toggleCoorDialog}
-                    >
-                      Assign Coordinator
-                    </Button>
+                    <></>
                   )}
                 </div>
               </CardBody>
             </Card>
           </Container>
-          {/* <Dialog
-            open={coorDialogOpen}
-            onClose={() => {
-              toggleCoorDialog();
-              fetchCoordinator();
-            }}
-            maxWidth="md"
-            fullWidth={true}
-          >
-            <DialogTitle>Assign Coordinator</DialogTitle>
-            <DialogContent style={{ height: "100%", minHeight: "400px" }}>
-              <CoorAssign
-                projectId={projectId}
-                projectName={title}
-                assignedCoorId={coordinator?.coorId}
-                toggleCoorDialog={toggleCoorDialog}
-              />
-            </DialogContent>
-          </Dialog>
-          <Dialog
-            open={tutorDialogOpen}
-            onClose={() => {
-              toggleTutorDialog();
-              fetchTutor();
-            }}
-            maxWidth="md"
-            fullWidth={true}
-          >
-            <DialogTitle>Assign Tutor</DialogTitle>
-            <DialogContent style={{ height: "100%", minHeight: "400px" }}>
-              <TutorAssign
-                projectId={projectId}
-                projectName={title}
-                assignedTutorId={tutor?.tutorId}
-                toggleTutorDialog={toggleTutorDialog}
-              />
-            </DialogContent>
-          </Dialog> */}
         </div>
       </div>
+      <Dialog open={open} onClose={handleClose}>
+        <div style={{ display: "flex" }}>
+          <DialogTitle style={{ minWidth: "36vw", paddingBottom: 2 }}>
+            Allocated Teams:
+          </DialogTitle>
+          <IconButton
+            aria-label="close"
+            onClick={handleClose}
+            sx={{
+              left: 4,
+              right: 0,
+              color: (theme) => theme.palette.grey[500],
+            }}
+          >
+            <CloseIcon />
+          </IconButton>
+        </div>
+        <Divider style={{ borderColor: "black" }} />
+        <DialogContent style={{ paddingTop: 0 }}>
+          <List sx={{ width: "100%" }}>
+            {currentTeam.length > 0 ? (
+              currentTeam.map((team) => {
+                return (
+                  <React.Fragment key={team.teamId}>
+                    <div style={{ display: "flex", direction: "row" }}>
+                      <ListItem
+                        alignItems="flex-start"
+                        style={{ flex: 7 }}
+                        onClick={() => {
+                          handleClickTeam(
+                            team.teamId,
+                            team.teamIdShow,
+                            team.teamName
+                          );
+                        }}
+                      >
+                        <ListItemText
+                          primary={`TeamName: ${team.teamName} `}
+                          secondary={
+                            <React.Fragment>
+                              <Typography
+                                sx={{ display: "inline" }}
+                                component="span"
+                                variant="body2"
+                                color="text.primary"
+                              >
+                                TeamId: {team.teamIdShow} <br />
+                              </Typography>
+                              <Typography
+                                sx={{ display: "inline" }}
+                                component="span"
+                                variant="body2"
+                                color="text.primary"
+                              >
+                                No. of TeamMembers: {team.teamMember.length}
+                              </Typography>
+                              <br />
+                              <Typography
+                                sx={{ display: "inline" }}
+                                component="span"
+                                variant="body2"
+                                color="text.primary"
+                              >
+                                TeamSkills:{" "}
+                                {team.teamSkills
+                                  ? team.teamSkills.join(", ")
+                                  : " "}{" "}
+                              </Typography>
+                            </React.Fragment>
+                          }
+                        />
+                      </ListItem>
+                    </div>
+                    <Divider component="li" />
+                  </React.Fragment>
+                );
+              })
+            ) : (
+              <Typography variant="h6" gutterBottom textAlign="center">
+                No Teams Found
+              </Typography>
+            )}
+          </List>
+        </DialogContent>
+      </Dialog>
     </main>
   );
 };
