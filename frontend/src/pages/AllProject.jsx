de5287e479c8c4e203de6f83d23e4db8e8d88d80
Zhenlon <zhenlongzhang7@gmail.com>
2024-07-12 20:54:12 +1000

    Add files via upload
    Added sidebar, but card info and adaptations need to be adjusted

--- frontend/src/pages/AllProject.jsx
+++ frontend/src/pages/AllProject.jsx
@@ -1,32 +1,27 @@
-// import axios from 'axios';
 import React, { useEffect, useRef, useState } from 'react';
+import { Outlet, useNavigate } from "react-router-dom";
 import { SearchOutlined } from '@ant-design/icons';
 import { Input, Card, Col, Row, Tag } from 'antd';
-import { useNavigate } from 'react-router-dom';
+import { Container } from "reactstrap";
+import axios from 'axios';
+import Sidebar from "../layouts/Sidebar";
+import Header from "../layouts/Header";
+import '../assets/scss/FullLayout.css'; // Make sure to import this file
 import '../styles/project.css';
-import { apiCall } from '../helper';
+import { apiCall } from '../helper'; // Make sure to import this file
 const { Meta } = Card;
-export default function Project() {
+
+const FullLayout = () => {
   const navigate = useNavigate();
+  const [loading, setLoading] = useState(false);
   const [team, setTeam] = useState(true);
-  const mounting = useRef(true);
   let [data, setData] = useState([]);
   const seachRef = useRef();
   const mountedRef = useRef(false);
+
   const loadMoreData = async () => {
     const url = 'v1/project/get/public_project/list';
-    // const params = {
-    //   name: 1,
-    // };
-    // if (seachRef.current.input.value)
-    //   params.name = seachRef.current.input.value;
-    const response = await apiCall(
-      'GET',
-      url,
-      null,
-      null,
-      null
-    );
+    const response = await apiCall('GET', url, null, null, null);
     console.log('....list....', response);
     if (response.error) {
       setData([]);
@@ -36,24 +31,15 @@ export default function Project() {
       setData([...res]);
     }
   };
-  // useEffect(() => {
-  //   if (mounting.current) {
-  //     mounting.current = false;
-  //   } else {
-  //     loadMoreData();
-  //   }
-  // }, [team]);
+
   useEffect(() => {
     if (!mountedRef.current) {
       mountedRef.current = true;
       console.log('...token..');
-      // if (!localStorage.getItem('token')) {
-      //   navigate('/login');
-      //   return;
-      // }
       loadMoreData();
     }
   }, [mountedRef]);
+
   const seachList = () => {
     const searchTerm = seachRef.current.input.value.toLowerCase();
     console.log(searchTerm);
@@ -72,98 +58,118 @@ export default function Project() {
             item.email,
             String(item.userId),
             item.userSkills,
-          ].some(
-            (field) => {
-              if (field) {
-                return field.toLowerCase().includes(searchTerm);
-              }
-            }
-            //
-          )
+          ].some((field) => field && field.toLowerCase().includes(searchTerm))
         );
       }
       console.log(filtered);
       setData(filtered);
     } else {
       console.log(data);
-      //   setData(data);
       loadMoreData();
     }
   };
+
   return (
-    <>
-      <div className="seach">
-        <Input
-          ref={seachRef}
-          size="large"
-          placeholder="Seach Projects"
-          prefix={<SearchOutlined />}
-        />
-        <div
-          style={{ marginLeft: '15px', cursor: 'pointer' }}
-          onClick={seachList}
-        >
-          Filter
+    <main>
+      <div className="pageWrapper d-lg-flex">
+        {/********Sidebar**********/}
+        <aside className="sidebarArea shadow" id="sidebarArea">
+          <Sidebar />
+        </aside>
+        {/********Content Area**********/}
+        <div className="contentArea">
+          <div className="d-lg-none headerMd">
+            {/********Header**********/}
+            <Header />
+          </div>
+          <div className="d-none d-lg-block headerLg">
+            {/********Header**********/}
+            <Header />
+          </div>
+          {/********Middle Content**********/}
+          <Container className="p-4 wrapper" fluid>
+            {/* Project Component content start */}
+            <div className="seach">
+              <Input
+                ref={seachRef}
+                size="large"
+                placeholder="Seach Projects"
+                prefix={<SearchOutlined />}
+              />
+              <div
+                style={{ marginLeft: '15px', cursor: 'pointer' }}
+                onClick={seachList}
+              >
+                Filter
+              </div>
+            </div>
+            <Row gutter={{ xs: 8, sm: 16, md: 24 }}>
+              {data.map((item, index) => (
+                <Col xs={24} sm={16} md={8} span={8} key={index}>
+                  <Card
+                    style={{ width: 300 }}
+                    cover={
+                      <div
+                        style={{
+                          background: '#D8BFD8',
+                          borderTopLeftRadius: '5px',
+                          borderTopRightRadius: '5px',
+                          height: '150px',
+                          display: 'flex',
+                          alignItems: 'center',
+                          justifyContent: 'center',
+                          fontWeight: '550',
+                          fontSize: '20px',
+                        }}
+                      >
+                        {item.title}
+                      </div>
+                    }
+                  >
+                    <Meta
+                      avatar={
+                        <div
+                          style={{
+                            width: '40px',
+                            height: '40px',
+                            borderRadius: '50%',
+                            background: '#D8BFD8',
+                          }}
+                        ></div>
+                      }
+                      title={
+                        <>
+                          {item.clientName}
+                          <br />
+                          <div style={{ color: '#ababab', fontSize: '12px' }}>
+                            {item.clientEmail}
+                          </div>
+                        </>
+                      }
+                      description={
+                        <>
+                          <div style={{ marginBottom: '10px' }}>
+                            {Array.isArray(item.requiredSkills) &&
+                              item.requiredSkills.map((skill, skillIndex) => (
+                                <Tag color="default" key={skillIndex}>
+                                  {skill}
+                                </Tag>
+                              ))}
+                          </div>
+                          {item.field}
+                        </>
+                      }
+                    />
+                  </Card>
+                </Col>
+              ))}
+            </Row>
+            {/* Project Component content end */}
+          </Container>
         </div>
       </div>
-      <Row gutter={{ xs: 8, sm: 16, md: 24 }}>
-        {data.map((item, index) => (
-          <Col xs={24} sm={16} md={8} span={8}>
-            <Card
-              style={{ width: 300 }}
-              cover={
-                <div
-                  style={{
-                    background: '#D8BFD8',
-                    borderTopLeftRadius: '5px',
-                    borderTopRightRadius: '5px',
-                    height: '150px',
-                    display: 'flex',
-                    alignItems: 'center',
-                    justifyContent: 'center',
-                    fontWeight: '550',
-                    fontSize: '20px',
-                  }}
-                >
-                  { item.title }
-                </div>
-              }
-            >
-              <Meta
-                avatar={
-                  <div
-                    style={{
-                      width: '40px',
-                      height: '40px',
-                      borderRadius: '50%',
-                      background: '#D8BFD8',
-                    }}
-                  ></div>
-                }
-                title={
-                  <>
-                    { item.clientName }
-                    <br />
-                    <div style={{ color: '#ababab', fontSize: '12px' }}>
-                    { item.clientEmail }
-                    </div>
-                  </>
-                }
-                description={
-                  <>
-                    <div style={{marginBottom: '10px'}}>
-            {Array.isArray(item.requiredSkills) && item.requiredSkills.map(skill => (
-              <Tag color='default'>{skill}</Tag>
-            ))}
-          </div>
-                    { item.field }
-                  </>
-                }
-              />
-            </Card>
-          </Col>
-        ))}
-      </Row>
-    </>
+    </main>
   );
-}
+};
+
+export default FullLayout;
