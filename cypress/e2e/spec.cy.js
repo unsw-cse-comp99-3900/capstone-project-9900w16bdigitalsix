cb1be33bfc8246c77a9ec9cb684154303de1b1a7
Zhenlon <your-email@example.com>
2024-08-01 22:52:45 +1000

    translation

--- cypress/e2e/spec.cy.js
+++ cypress/e2e/spec.cy.js
@@ -1,34 +1,34 @@
 describe('My First Test', () => {
   it('Visits the Login Page and Enters Credentials', () => {
-    // 访问登录页面
+    // Visit the login page
     cy.visit('http://localhost:3000/login')
 
-    // 输入 email 和 password
+    // Input Email and Password
     cy.get('#email').type('tutor_1@ad.au')
     cy.get('#password').type('admin123')
 
-    // 使用按钮的 id 选择并点击登录按钮
+    // Click on the Login button
     cy.get('#buttonLogin').click()
 
-    // 验证是否跳转到预期的页面
+    // Verify that it directs to the expected page
     cy.url().should('include', '/project/allproject')
 
-    // 搜索框输入
+    // Search box input
     cy.get('Input').type('Cloud').should('have.value', 'Cloud')
 
-    //点击filter按钮
+    //click filter button
     cy.contains('button', 'Filter').click()
 
-    //点击搜索后的项目
+    //Click on the item after searching
     cy.get('.custom-card-header').click()
     
-    //点击Allocatedteams按钮
+    //click Allocatedteams button
     cy.contains('button', 'Allocated Teams').click()
 
-    //点击team
+    //click team
     cy.contains('p.MuiTypography-root', 'TeamId: 207003').click()
 
-    //打分
+    //grade
     cy.get('.ant-btn.ant-btn-primary')
     .filter((index, element) => {
       return element.querySelector('.anticon-star') !== null;
@@ -55,41 +55,41 @@ describe('My First Test', () => {
       .type('good')
     cy.contains('button', 'Save').click()
 
-    //查看得分
+    //View Score
     cy.get('button .anticon-file-text').click()
     
-    //点击消息
+    //click Notification
     cy.contains('a', 'Notification').click()
 
-    // 验证点击后的行为，例如页面是否跳转到 /notification
+    // Validates the behaviour after a click, whether the page directs to the /notification
     cy.url().should('include', '/notification')
 
-    //点击team
+    //click team
     cy.contains('a', 'Team').click()
 
-    // 验证点击后的行为，例如页面是否跳转到/team/tutor
+    // Validates the behaviour after a click, whether the page directs to the/team/tutor
     cy.url().should('include', '/team/tutor')
     
-    //搜索框检查
+    //Search box checking
     cy.get('input[placeholder="Search Team"]')
       .clear()
       .type('team_7')
 
-    // 验证输入内容是否正确
+    // Verify that the input is correct
     cy.get('input[placeholder="Search Team"]').should('have.value', 'team_7')
 
-    //点击filter按钮并且清除
+    //Click the filter button and clear 
     cy.contains('button', 'Filter').click()
     
-    //区分COMP9900和COMP3900
+    //Distinguish between COMP9900 and COMP3900
     cy.get('div[role="combobox"][id="course"]').click()
     cy.get('li[data-value="COMP3900"]').click()
     cy.get('li[data-value="COMP9900"]').click()
 
-    //Tutor查看student list
+    //Tutor check student list
     cy.contains('button', 'STUDENT LIST').click()
 
-    //Tutor查看student信息
+    //Tutor check student信息
     cy.get('input[placeholder="Search Student"]')
       .clear()
       .type('3')
@@ -98,7 +98,7 @@ describe('My First Test', () => {
     cy.get('li[data-value="COMP3900"]').click()
     cy.get('li[data-value="COMP9900"]').click()
 
-    //Tutor查看Unallocated Team List
+    //Tutor check Unallocated Team List
     cy.contains('button', 'UNALLOCATED TEAMS').click()
     cy.get('div[role="combobox"][id="course"]').click()
     cy.get('li[data-value="COMP3900"]').click()
@@ -108,7 +108,7 @@ describe('My First Test', () => {
       .type('4')
     cy.get('input[placeholder="Search Unallocated Team"]').should('have.value', '4')
 
-    //Message功能
+    //Message
     cy.contains('a', 'Message').click()
     cy.url().should('include', '/message')
     cy.get('button.ant-btn-primary.list-item-button').contains('+ New Channel').click()
@@ -117,22 +117,22 @@ describe('My First Test', () => {
       .type('s')
     cy.get('input.ant-checkbox-input[value="11"]').click()
 
-    //发送信息
+    //send message
     cy.get('input.ant-checkbox-input[value="11"]').should('be.checked')
     cy.contains('button', 'Invite').click()
     cy.get('input#exampleFormControlInput2')
       .clear()
       .type('Test{enter}')
 
-    // 验证输入内容是否正确
+    // Verify that the input is correct
     cy.get('input#exampleFormControlInput2').should('have.value', 'Test')
 
-    //发送名片
+    //Give a name card
     cy.get('button.MuiButtonBase-root.MuiIconButton-root.circle-buttonshare').click()
     cy.get('input.ant-radio-input[value="8"]').click()
     cy.contains('button', 'Share').click()
     
-    //群聊功能
+    //Group Chat function
     cy.get('button.ant-btn-primary.list-item-button').contains('+ New Channel').click()
     cy.get('input[placeholder="Search by name or email"]')
       .clear()
@@ -143,26 +143,26 @@ describe('My First Test', () => {
       .clear()
       .type('Test{enter}')
     
-    //查看群成员
+    //View Group Members
     cy.get('button.MuiButtonBase-root.MuiIconButton-root.group-icon-button').click()
     cy.get('.ant-modal-wrap').last().find('svg[data-icon="close"]').click({ force: true })
 
-    //群邀请
+    //group invitation
     cy.contains('button', '+ Invite').click()
     cy.contains('button', 'Cancel').click()
 
-    //切换频道
+    //Switching channels
     cy.get('button.ant-btn-primary.list-item-button').contains('span', 'All Channel').click()
     cy.contains('li.ant-list-item', 'Private Chat: tutor_1 and Lisa0').click()
 
-    //清除消息
+    //Clear Message
     cy.contains('a', 'Notification').click()
     cy.get('button.ant-btn-primary')
       .find('span[role="img"][aria-label="close"]')
       .click({ force: true })
     cy.contains('button.ant-btn-primary.ant-btn-dangerous', 'Clear').click({ force: true })
 
-    //编辑profile
+    // Edit profile
     cy.contains('a', 'Profile').click()
     cy.get('button.MuiButtonBase-root.MuiButton-root.MuiButton-containedPrimary')
       .contains('Edit')
@@ -172,12 +172,12 @@ describe('My First Test', () => {
       .clear()
       .type('Hello everyone！')
 
-    // 验证输入后的文本内容
+    // Validating text content after input
     cy.get('textarea.MuiInputBase-input.MuiOutlinedInput-input.MuiInputBase-inputMultiline')
       .first()
       .should('have.value', 'Hello everyone！')
     
-    //保存
+    //save
     cy.get('button.MuiButtonBase-root.MuiButton-root.MuiButton-containedPrimary')
       .contains('Save')
       .click()
