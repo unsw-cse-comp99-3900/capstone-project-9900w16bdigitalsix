79725ee52803c7e7ed59e7740a63b6acc312cafd
Xue Cuiling <xuecuiling9909@gmail.com>
2024-08-02 19:36:34 +1000

    change user not found error

--- backend/web/controllers/user.go
+++ backend/web/controllers/user.go
@@ -82,8 +82,8 @@ func PasswordLogin(ctx *gin.Context) {
 			switch e.Code() { // 把 grpc 的 code 转换成 HTTP 的状态码
 			case codes.NotFound:
 				ctx.JSON(http.StatusNotFound, gin.H{
-					// "error": "User not found",
-					"error": "Account and password do not match",
+					"error": "User not found",
+					// "error": "Account and password do not match",
 				})
 			default:
 				ctx.JSON(http.StatusInternalServerError, gin.H{
